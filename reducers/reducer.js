e147fdb6ea64eb6a04e7f75125e8a39ce3804ffa
Vyacheslav Slinko <vslinko@yahoo.com>
2016-01-09 01:53:55 +0300

    Update reducer.js

--- reducers/reducer.js
+++ reducers/reducer.js
@@ -1,21 +1,20 @@
 import { KEY_UPDATE } from '../constants/ActionTypes'
 
-let initialData     =  [
+const initialData = [
   { id: 0, index: 1, firstName: 'Click-me', lastName: 'London1', country: 'jon@gmail.com1'},
   { id: 1, index: 2, firstName: 'Click-me', lastName: 'London2', country: 'jon@gmail.com2'},
-  { id: 2, index: 3, firstName: 'Click-me', lastName: 'London3', country: 'jon@gmail.com3'}
+  { id: 2, index: 3, firstName: 'Click-me', lastName: 'London3', country: 'jon@gmail.com3'},
 ];
 
-
-export default function reducer(state = {data: initialData}, action) {
-
+export default function reducer(state = initialData, action) {
   switch (action.type) {
     case KEY_UPDATE:
-      const newdata=state.data.map((item, num)=>(
-        num==action.rowId?{...item, firstName: item.firstName+'-more'}:item
-      ))
-      state={...state, data: newdata};
       return state
+        .map((item, num) => (
+          num === action.rowId
+            ? { ...item, firstName: item.firstName + '-more' }
+            : item
+        ))
 
     default:
       return state
