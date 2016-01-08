42306ffae7781c4ad61ae2079d5079bcb93caafd
Vyacheslav Slinko <vslinko@yahoo.com>
2016-01-09 01:29:24 +0300

    Update CustomCell.js

--- components/CustomCell.js
+++ components/CustomCell.js
@@ -1,8 +1,14 @@
-import React, { Component, PropTypes } from 'react'
-import classnames from 'classnames'
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 export default class CustomCell extends React.Component {
-  render(){
-    return (<div className="custom-row-card"><strong><a onClick={this.props.onElementClick}>{this.props.data}</a></strong></div>);
+  render() {
+    return (
+      <div className="custom-row-card">
+        <strong>
+          <a onClick={this.props.onElementClick}>{this.props.data}</a>
+        </strong>
+      </div>
+    );
   }
 };
