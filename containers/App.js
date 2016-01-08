316672533cfe6f86b6bfcc03beade847d1ac3580
Vyacheslav Slinko <vslinko@yahoo.com>
2016-01-09 01:35:14 +0300

    Update App.js

--- containers/App.js
+++ containers/App.js
@@ -1,39 +1,42 @@
 import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
 import Griddle from 'griddle-react'
-require('react-datagrid/index.css')
+import 'react-datagrid/index.css'
 import CustomGrid from './CustomGrid'
 import CustomCell from './CustomCell'
 
+function rowStyle(data, props) {
+  const style = {};
+  
+  if (props.index % 4 === 0) {
+    style.color = 'green';
+  }
+  
+  if (data.country === 'USA') {
+    style.background = '#FFD3D3';
+    style.fontWeight = 'bold';
+  }
+  
+  return style;
+}
+
+const columns = [
+  { name: 'index', title: '#', width: 50 },
+  { name: 'firstName', render: (cellValue, rowObject) => <CustomCell data={cellValue} id={rowObject.id} /> },
+  { name: 'lastName'  },
+  { name: 'country' }
+]
+
 export default class App extends React.Component {
-  render (){
-    function rowStyle(data, props){
-      var style = {}
-      if (props.index % 4 == 0){
-        style.color = 'green'
-      }
-      if (data.country == 'USA'){
-        style.background = '#FFD3D3'
-        style.fontWeight = 'bold'
-      }
-      return style
-    }
-    var columns = [
-      { name: 'index', title: '#', width: 50 },
-      { name: 'firstName', render: function(cellValue,rowObject){return <CustomCell data={cellValue} id={rowObject.id}/>} },
-      { name: 'lastName'  },
-      { name: 'country' }
-    ]
-    return <div>
-      <CustomGrid
-          idProperty='id'
+  render() {
+    return (
+      <div>
+        <CustomGrid
+          idProperty="id"
           rowStyle={rowStyle}
           columns={columns}
-          setState={this.setState}
-      />
-    </div>;
-  }
-  constructor(props, context) {
-    super(props, context);
+        />
+      </div>
+    );
   }
 }
